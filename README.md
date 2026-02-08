bbc667132a67ac7b37233354e9047819ce8b3eec
Qinchuanqi <2254943770@qq.com>
2026-02-08 20:31:56 +0800

    修改阿里云的key和secret的载入方式

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ReggieTakeOut
+1.1 套餐功能 - 在更新时删除照片✅
+1.2 小程序短信验证登录功能✅
+1.3 LoginCheckFilter中的BaseContext.setCurrentUserId存在问题：已解决，若管理端的程序和顾客端的程序不在一个浏览器上登录，则不会出现顾客端的当前ID被设为管理端的情况。✅
+
+1.4 新增地址之后点击设为默认地址会先消失✅
+1.5 add-order界面里的获取地址没有实时刷新✅
+1.6 把设置默认地址和对整个item的点击拆开✅
+1.7 不只是简单的把订单地址设成默认地址✅
+
+1.8 order确认之后，购物车里的内容是否清空✅
+
+1.9 实现了aliyun的key和secret的动态载入, 通过application.yml中的spring.config.import.optional:classpath:.env[.properties]来实现✅
+
--- src/main/java/org/example/reggie/common/AliyunDyPnsClientUtil.java
+++ src/main/java/org/example/reggie/common/AliyunDyPnsClientUtil.java
@@ -4,14 +4,17 @@ package org.example.reggie.common;
 import com.aliyun.dypnsapi20170525.Client;
 import com.aliyun.teaopenapi.models.Config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
 
 
 /**
  * 阿里云短信服务客户端工具类
  */
 @Component
+@Slf4j
 public class AliyunDyPnsClientUtil {
     /**
      * 阿里云短信服务AccessKeyId
@@ -25,9 +28,14 @@ public class AliyunDyPnsClientUtil {
     @Value("${aliyun.access-secret}")
     private String accessKeySecret;
 
+
+
     // 初始化客户端
     // 调用带 throws Exception 的方法：调用者必须使用 try-catch 块捕获异常，或者继续向上抛出异常
     public Client createClient() throws Exception {
+
+        log.info(String.format("key:%s,secret:%s", accessKeyId, accessKeySecret));
+
         Config config = new Config()
                 .setAccessKeyId(accessKeyId)
                 .setAccessKeySecret(accessKeySecret);
